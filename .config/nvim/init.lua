c7eda9f092a14595168fa75ad0049d17c5f54c75
Rad Cirskis <nad2000@gmail.com>
2026-03-10 15:50:56 +1300

    so far so good

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,7 +20,8 @@ vim.opt.sidescrolloff = 3
 vim.opt.wrapscan = true
 vim.opt.encoding = "UTF-8"
 vim.opt.splitbelow = true
-vim.opt.clipboard:append("unnamedplus")
+-- vim.opt.clipboard:append("unnamedplus")
+vim.opt.clipboard = "unnamedplus"
 
 -- hybrid line number mode
 if not is_view then
@@ -332,6 +333,7 @@ end
 
 -- Cipboard for all operations
 -- set clipboard+=unnamedplus  " better to user '*' or '+' register
+--[[
 vim.g.clipboard = {
   name = 'myClipboard',
   copy = {
@@ -344,6 +346,7 @@ vim.g.clipboard = {
   },
   cache_enabled = 1,
 }
+]]
 
 -- Go to the last cursor location when a file is opened
 vim.api.nvim_create_autocmd("BufReadPost", {
